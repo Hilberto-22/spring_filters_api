df3baae3bcffe1193ade8f4f593178fbdf96b31c
hilberto <betogouveia1@outlook.com>
2023-06-06 17:05:45 -0300

    Initial commit

---
+++ src/main/java/com/spring/filtros/domain/Supplier.java
@@ -0,0 +1,22 @@
+package com.spring.filtros.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_supplier")
+public class Supplier implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false)
+    private String name;
+}
---
+++ src/test/java/com/spring/filtros/FiltrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.filtros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
